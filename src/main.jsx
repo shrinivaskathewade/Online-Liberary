994baa1565e1cfc4bcfca5ec4ec27c4b8cae8dc7
Shriniavs-kathewade <shripatil676@gmail.com>
2026-01-12 17:55:53 +0530

    Basic routing added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
-import './index.css'
+import Home from './pages/Home'
+import BrowseBooks from './pages/BrowseBooks'
+import AddBook from './pages/AddBook'
+
+const router = createBrowserRouter([
+  {
+    path : "",
+    element : <App />
+  },
+  { path: '/',
+     element: <Home /> 
+    },
+
+  { path: '/books',
+    element: <BrowseBooks /> 
+  },
+
+  { path: '/add-book',
+     element: <AddBook /> 
+    },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
