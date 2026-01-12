ec409b65da1c87b9b371c4d2131069a40c46412e
Shriniavs-kathewade <shripatil676@gmail.com>
2026-01-12 17:50:54 +0530

    Created basic pages

---
+++ src/pages/AddBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddBook = () => {
+  return (
+    <div>
+      <h2>Add Books</h2>
+    </div>
+  )
+}
+
+export default AddBook
---
+++ src/pages/BrowseBooks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BrowseBooks = () => {
+  return (
+    <div>
+      <h2>Browse Books</h2>
+    </div>
+  )
+}
+
+export default BrowseBooks
